df61915ac300829d5b12e0b66d0ee268cf5ed32e
Tomas <tomasaintavit@gmail.com>
2022-03-25 12:31:07 -0300

    add No cache to browser

--- myApp.js
+++ myApp.js
@@ -8,6 +8,7 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
 let ninentyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninentyDaysInSeconds, force: true}));
